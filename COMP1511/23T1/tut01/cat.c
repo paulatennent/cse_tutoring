b9fa0eb3032ff9ec4a6385730e51d504a6e10017
Paula Tennent <pjtennent@gmail.com>
2023-03-15 12:39:57 +1100

    adding cat.c

---
+++ COMP1511/23T1/tut01/cat.c
@@ -0,0 +1,11 @@
+// written by Paula (2023)
+
+#include <stdio.h>
+
+int main(void) {
+  // remember, to print out '\', we need to write '\\' .
+  printf(" /\\___/\\\n");
+  printf("(=' w '=)\n");
+
+  return 0;
+}
