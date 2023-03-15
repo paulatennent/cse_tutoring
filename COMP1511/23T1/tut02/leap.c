678f874ffcfe991d12f37422a1be60ff472fd428
Paula Tennent <pjtennent@gmail.com>
2023-03-15 12:31:01 +1100

    adding leap code

---
+++ COMP1511/23T1/tut02/leap.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+
+  printf("Enter a year: ");
+  int year;
+  scanf("%d", &year);
+
+  if (year % 4 == 0) {
+
+    if (year % 100 == 0) {
+
+      if (year % 400 == 0) {
+        printf("Yes a leap year\n");
+      } else {
+        printf("Not a leap year\n");
+      }
+
+    } else {
+      printf("Yes a leap year\n");
+    }
+
+  } else {
+    printf("Not a leap year\n");
+  }
+
+
+  return 0;
+}
