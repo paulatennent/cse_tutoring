ca10cc0e511287b4be0b6687df7f224cb53e913a
Paula Tennent <pjtennent@gmail.com>
2023-05-31 23:35:38 +1000

    tut1 files

---
+++ COMP1511/23T2/tut01/helloworld.c
@@ -0,0 +1,11 @@
+// A short program that prints "hello world"
+// Written by Paula Tennent (2023)
+
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("Hello World\n");
+
+    return 0;
+}
---
+++ COMP1511/23T2/tut01/print_cat.c
@@ -0,0 +1,12 @@
+// A short program that prints a cute ascii cat!
+// Written by Paula Tennent (2023)
+
+#include <stdio.h>
+
+int main(void) {
+    
+    printf(" /\\___/\\\n");
+    printf("(=' w '=)\n");
+
+    return 0;
+}
