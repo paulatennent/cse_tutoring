41456bcd188c9ff5993c54d7a36323d0194d4b1a
Paula Tennent <pjtennent@gmail.com>
2023-06-21 21:57:58 +1000

    updated W4 stuff to make it better

---
+++ COMP1511/23T2/tut04/function_exercises.c
@@ -0,0 +1,34 @@
+// Written by Paula Tennent (2023)
+
+#include <stdio.h>
+
+int main(void) {
+
+    
+    return 0;
+}
+
+// EASY: Write a function that returns 1 (aka true) if a number is even
+// or 0 (aka false) if the number is odd
+
+
+
+// MEDIUM: Write a function that takes in a char and returns the uppercase
+// version of that char 
+
+
+
+// HARD: Write a function that takes in a struct colour and finds the
+// average of the RGB values
+
+// NOTE 1: the average formula is sqrt(red) + sqrt(green) + sqrt(blue)
+// NOTE 2: you have access too the double sqrt(int num); function
+
+struct colour {
+    int red;
+    int green;
+    int blue;
+}; // note, make sure to move this above the main if you want it to compile
+
+
+
---
+++ COMP1511/23T2/tut04/functions_demo.c
@@ -0,0 +1,18 @@
+// Written by Paula Tennent (2023)
+
+#include <stdio.h>
+
+int main(void) {
+
+    // find the average of two numbers
+    int mark1 = 60;
+    int mark2 = 70;
+
+    int final_mark = (mark1 + mark2) / 2.0;
+
+    printf("Your final mark is %d\n", final_mark);
+
+
+
+
+}
