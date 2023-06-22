220d5eba71213628b3fb643fcc19a6be330cd31e
Paula Tennent <pjtennent@gmail.com>
2023-06-22 23:11:23 +1000

    added array code

--- COMP1511/23T2/tut04/arrays.c
+++ COMP1511/23T2/tut04/arrays.c
@@ -2,42 +2,77 @@
 
 #include <stdio.h>
 
+#define MAX 100
 
 // add 1 to any even values -> the arrays should now be all ODD!
-??? odd_only(???) {
-    // TODO
+void odd_only(int array[], int length) {
+    int i = 0;
+    while (i < length) {
+        if (array[i] % 2 == 0) {
+            array[i] += 1;
+        }
+        i++;
+    }
 }
 
 // given 2 arrays, copy the values from the first array
 // into the second
-??? copy_array_values(???) {
-    // TODO
+void copy_array_values(int array1[], int array2[], int length) {
+    int i = 0;
+    while (i < length) {
+        array2[i] = array1[i];
+        i++;
+    }
 }
 
 // find the largest number in the array, return it
-??? largest_number(???) {
-    // TODO	
+int largest_number(int array[], int length) {
+    int i = 0;
+    int max = array[0];
+    while (i < length) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+        i++;
+    }
+    return max;
+}
+
+// prints out an array
+void print_array(int array[], int length) {
+    int i = 0;
+    printf("[");
+    while (i < length) {
+        if (i != 0) {
+            printf(" ");
+        }
+        printf("%d", array[i]);
+        i++;
+    }
+    printf("]\n");
 }
 
 int main(void) {
 
     // create an array
-
+    int numbers[MAX] = {1, 2, 4, 5, 6};
+    print_array(numbers, 5);
 
     // pass it into odd_only();
-    odd_only();
+    odd_only(numbers, 5);
 
+    print_array(numbers, 5);
 
     // create a second array (make it empty or all 0's)
-
+    int numbers2[MAX] = {0};
 
     // pass the two arrays into copy_array_values
-    copy_array_values();
+    copy_array_values(numbers, numbers2, 5);
 
 
     // find the largest number in the second array
-    largest_number();
-    printf("The largest number is: %d", ???);
+    int largest = largest_number(numbers2, 5);
+    printf("The largest number is: %d\n", largest);
 
     return 0;
 }
