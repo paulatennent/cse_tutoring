8bd6be7e98d57e91b2902690838093c83e89503c
Paula Tennent <z5255101@unsw.edu.au>
2024-03-01 10:51:16 +1100

    updates

--- COMP1511/23T3/tut03/demo.c
+++ COMP1511/23T3/tut03/demo.c
@@ -3,26 +3,15 @@
 int main(void) {
 
 
+    printf("he");
+    
     int i = 0;
-    while (i < 10) {
-
-        /// print helllllo
-        int j = 0;
-        while (j < 10) {
-
-            if (i == j * j) {
-                printf("*");
-            } else {
-                printf("_");
-            }
-            
-            j++;
-        }
-        printf("\n");
-        // end of word
-
+    while (i < 10){
+        printf("l");
         i++;
     }
+
+    printf("o\n");
 
     return 0;
 }
