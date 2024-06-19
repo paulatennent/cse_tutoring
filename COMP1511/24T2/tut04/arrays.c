eccaf5324f1bcbb5a51daec8c818a23921edc57a
Paula Tennent <pjtennent@gmail.com>
2024-06-20 08:56:31 +1000

    adding lab 04

---
+++ COMP1511/24T2/tut04/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  // create an array with 5 elements
+
+  // print it out
+  
+  // add 1 to each even element
+  
+  // print it out again
+  
+  return 0;
+}
