edd371187c712fe6491227181192c8ee6a1fd760
Paula Tennent <pjtennent@gmail.com>
2024-06-27 08:32:21 +1000

    tut 5 updates

---
+++ COMP1511/24T2/tut05/2Darray.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    // createa a 2D array
+    
+    // set it all to be 1
+    
+    // add some other items... :/
+
+    // square everything in the array (with a function...)
+
+    // print it out (also with a function)
+    
+    // change it over to a 
+
+    return 0;
+}
---
+++ COMP1511/24T2/tut05/strings.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // create a string
+    
+    // print it out
+
+    // change the string
+
+    // print out only till the first space
+
+    // scan in a string
+
+    return 0;
+}
